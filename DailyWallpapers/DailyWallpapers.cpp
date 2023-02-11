da4a54edf1f81d109b461e1a7900f764a7771b12
無冬 <123821243+aisyshk@users.noreply.github.com>
2023-02-10 18:53:32 -0700

    trycatch

--- DailyWallpapers/DailyWallpapers.cpp
+++ DailyWallpapers/DailyWallpapers.cpp
@@ -31,39 +31,46 @@ int main()
     ::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 
     do {
-        if (S_OK == URLDownloadToFile(NULL, srcUrl, jsonFile, 0, NULL))
+        try
         {
-            std::cout << "Saved to " << jsonFile << "\n\n";
+            if (S_OK == URLDownloadToFile(NULL, srcUrl, jsonFile, 0, NULL))
+            {
+                std::cout << "Saved to " << jsonFile << "\n\n";
+            }
+            else { std::cout << "Failed to acquire file.\n"; }
+
+            Sleep(1000);
+
+            std::ifstream f(jsonFile);
+            json data = json::parse(f);
+
+            std::string cleanedUp;
+            cleanedUp = to_string(data["images"][0]["url"]);
+            cleanedUp.erase(remove(cleanedUp.begin(), cleanedUp.end(), '\"'));
+            bingUrl.append(cleanedUp);
+
+            std::wstring stemp = std::wstring(bingUrl.begin(), bingUrl.end());
+            LPCWSTR sw = stemp.c_str();
+
+            if (S_OK == URLDownloadToFile(NULL, sw, fileName, 0, NULL))
+                std::cout << "Saved file to " << fileName << "\n";
+            else
+                std::cout << "Unable to acquire file.\n";
+
+            Sleep(1000);
+
+            BOOL success = SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, (void*)fileName, SPIF_UPDATEINIFILE);
+
+            if (!success)
+                std::cout << GetLastError();
+            else
+                std::cout << "Successfully changed background!\n";
+
+            std::this_thread::sleep_for(std::chrono::hours(1));
         }
-        else { std::cout << "Failed to acquire file.\n"; }
-
-        Sleep(1000);
-
-        std::ifstream f(jsonFile);
-        json data = json::parse(f);
-
-        std::string cleanedUp;
-        cleanedUp = to_string(data["images"][0]["url"]);
-        cleanedUp.erase(remove(cleanedUp.begin(), cleanedUp.end(), '\"'));
-        bingUrl.append(cleanedUp);
-
-        std::wstring stemp = std::wstring(bingUrl.begin(), bingUrl.end());
-        LPCWSTR sw = stemp.c_str();
-
-        if (S_OK == URLDownloadToFile(NULL, sw, fileName, 0, NULL))
-            std::cout << "Saved file to " << fileName << "\n";
-        else
-            std::cout << "Unable to acquire file.\n";
-
-        Sleep(1000);
-
-        BOOL success = SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, (void*)fileName, SPIF_UPDATEINIFILE);
-
-        if (!success)
-            std::cout << GetLastError();
-        else
-            std::cout << "Successfully changed background!\n";
-
-        std::this_thread::sleep_for(std::chrono::hours(1));
+        catch (std::exception e)
+        {
+            std::cerr << e.what();
+        }
     } while (true);
 }
